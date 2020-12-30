bafdcea0d056d6b09b831e6758b2e86f393949d2
Gherciu Gheorghe <gherciu553@gmail.com>
2020-12-30 17:36:30 +0200

    fix: what

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const { eslintConfig } = require('@episclera/configkit')
 
 module.exports = eslintConfig
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const { commitlintConfig } = require('@episclera/configkit')
 
 module.exports = commitlintConfig
