0d5cb8b936f96ac0eff21f4bc82369498972dadd
Gherciu Gheorghe <gherciu553@gmail.com>
2021-05-21 22:11:56 +0300

    feat: add lint staged conf

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,3 @@
-module.exports = {
-  '*.(ts|tsx)': ['npm run lint:packages'],
-  '*.(less)': ['npm run lint:packages:styles'],
-}
+const { lintStagedConfig } = require('@episclera/multipack')
+
+module.exports = lintStagedConfig
