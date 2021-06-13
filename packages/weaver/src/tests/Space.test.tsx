90a4927817d8dafcfe0e526c4a858e27776741d6
Gherciu Gheorghe <gherciu553@gmail.com>
2021-06-14 01:41:22 +0300

    feat: add space element

---
+++ packages/weaver/src/tests/Space.test.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Space from '../Space'
+
+describe('Space', () => {
+  it('Should render the children element and wrap it in space specific class', () => {
+    const { getByTestId } = render(
+      <Space>
+        <div data-testid='space-children' />
+      </Space>,
+    )
+
+    expect(getByTestId('space-children')).toBeTruthy()
+    expect(
+      (getByTestId('space-children').parentElement as HTMLDivElement).classList,
+    ).toContain('ant-space-item')
+  })
+
+  it('Should render by default a space with direction vertical', () => {
+    const { getByTestId } = render(
+      <Space>
+        <div />
+      </Space>,
+    )
+
+    expect(getByTestId('space')).toBeTruthy()
+    expect(getByTestId('space').classList).toContain('ant-space-vertical')
+  })
+
+  it('Should render by default each non last child with size large', () => {
+    const { getByTestId } = render(
+      <Space>
+        <div />
+        <div />
+      </Space>,
+    )
+
+    expect(getByTestId('space')).toBeTruthy()
+    expect(
+      (
+        getByTestId('space').querySelector(
+          '.ant-space-item:first-of-type',
+        ) as HTMLDivElement
+      ).style.marginBottom,
+    ).toBe('24px')
+    expect(
+      (
+        getByTestId('space').querySelector(
+          '.ant-space-item:last-of-type',
+        ) as HTMLDivElement
+      ).style.marginBottom,
+    ).toBe('')
+  })
+})
--- packages/weaver/types/index.d.ts
+++ packages/weaver/types/index.d.ts
@@ -1,17 +1,19 @@
 import React from 'react'
-import { RowProps } from 'antd'
+import { RowProps, SpaceProps } from 'antd'
 import { LinkProps as NextLinkProps } from 'next/link'
 
 // Antd and Next overridden components
 export type ContainerProps = React.HTMLAttributes<HTMLDivElement>
 export type TContainer = React.FC<ContainerProps>
 
-export type TRow = React.FC<RowProps>
-
 export interface LinkProps
   extends React.HTMLAttributes<HTMLAnchorElement>,
     NextLinkProps {}
 export type TLink = React.FC<LinkProps>
+
+export type TRow = React.FC<RowProps>
+
+export type TSpace = React.FC<SpaceProps>
 
 // export packages
 export * from 'antd'
@@ -19,5 +21,6 @@ export * from '@episclera/weaver-icons'
 export * from '@episclera/weaver-utils'
 export * from '@episclera/weaver-theme'
 export const Container: TContainer
-export const Row: TRow
 export const Link: TLink
+export const Row: TRow
+export const Space: TSpace
